0a96b41bf443326583df0231c407622c534c3361
unknown <pedro.failtnt@hotmail.com>
2018-07-02 21:14:35 -0300

    garantia

---
+++ app/src/main/java/com/example/aula_01/horadospases/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.aula_01.horadospases;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pressionarBotao(View view){
+        Intent selecionarActivity = new Intent(this,SelecionarActivity.class);
+        startActivity(selecionarActivity);
+
+    }
+}
