7577233db35a2f4098e9cd36ae365ed8cca9fd95
unknown <pedro.failtnt@hotmail.com>
2018-07-05 22:42:06 -0300

    terceira tela pronta mas com erro

--- app/src/main/java/com/example/aula_01/horadospases/UltimaTelaActivity.java
+++ app/src/main/java/com/example/aula_01/horadospases/UltimaTelaActivity.java
@@ -1,5 +1,6 @@
 package com.example.aula_01.horadospases;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,12 +10,171 @@ public class UltimaTelaActivity extends AppCompatActivity {
     String minuto;
     String ampm;
 
-    TextView textView_;
-    TextView sobrenome2;
-    TextView idade2;
+    TextView textView_franca;
+    TextView textView_china;
+    TextView textView_africa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultima_tela);
+
+        Intent intent = getIntent();
+        Bundle meuBundle = intent.getExtras();
+
+        hora = meuBundle.getString("hora");
+        minuto = meuBundle.getString("minuto");
+        ampm = meuBundle.getString("ampm");
+
+        hora.toString();
+        minuto.toString();
+        ampm.toString();
+
+        textView_franca = (TextView) findViewById(R.id.textView_franca);
+        textView_china = (TextView) findViewById(R.id.textView_china);
+        textView_africa = (TextView) findViewById(R.id.textView_africa);
+
+        textView_franca.setText(franca(hora,minuto,ampm));
+        textView_china.setText(china(hora,minuto,ampm));
+        textView_africa.setText(africa(hora,minuto,ampm));
+
+
+    }
+
+    public String franca(String x,String y, String z){
+        int auxx;
+        auxx = Integer.parseInt(x);
+        auxx += 4;
+        if(auxx > 12){
+            if(auxx <= 8) {
+                auxx += -12;
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return "0"+hora_final+" : "+y+" "+z;
+            }else{
+                auxx += -12;
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return hora_final+" : "+y+" "+z;
+            }
+        }else{
+            if(auxx <= 8) {
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return "0"+hora_final+" : "+y+" "+z;
+            }else{
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return hora_final+" : "+y+" "+z;
+            }
+
+
+        }
+
+
+    }
+
+    public String china(String x,String y, String z){
+        int auxx;
+        auxx = Integer.parseInt(x);
+        auxx += 11;
+        if(auxx > 12){
+            if(auxx <= 8) {
+                auxx += -12;
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return "0"+hora_final+" : "+y+" "+z;
+            }else{
+                auxx += -12;
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return hora_final+" : "+y+" "+z;
+            }
+        }else{
+            if(auxx <= 8) {
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return "0"+hora_final+" : "+y+" "+z;
+            }else{
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return hora_final+" : "+y+" "+z;
+            }
+
+
+        }
+
+
+
+    }
+
+    public String africa(String x,String y, String z){
+        int auxx;
+        auxx = Integer.parseInt(x);
+        auxx += 5;
+        if(auxx > 12){
+            if(auxx <= 8) {
+                auxx += -12;
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return "0"+hora_final+" : "+y+" "+z;
+            }else{
+                auxx += -12;
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return hora_final+" : "+y+" "+z;
+            }
+        }else{
+            if(auxx <= 8) {
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return "0"+hora_final+" : "+y+" "+z;
+            }else{
+                if(z.equalsIgnoreCase("AM")){
+                    z = "PM";
+                }else{z = "AM";}
+
+                String hora_final = Integer.toString(auxx);
+                return hora_final+" : "+y+" "+z;
+            }
+
+
+        }
+
+
+
     }
 }
